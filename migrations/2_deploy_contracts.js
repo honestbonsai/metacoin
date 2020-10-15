3c2d60d38a14298d933da84f54b5fdbfdd183d54
HL <>
2020-10-15 13:19:56 -0400

    Add more contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,19 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
+const MetaCoin2 = artifacts.require("MetaCoin2");
+const MetaCoin3 = artifacts.require("MetaCoin3");
+const MetaCoin4 = artifacts.require("MetaCoin4");
+const MetaCoin5 = artifacts.require("MetaCoin5");
+
+const contracts = [
+  MetaCoin, MetaCoin2, MetaCoin3, MetaCoin4, MetaCoin5
+];
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+
+  contracts.forEach(contract => {
+    deployer.link(ConvertLib, contract);
+    deployer.deploy(contract);
+  });
 };
