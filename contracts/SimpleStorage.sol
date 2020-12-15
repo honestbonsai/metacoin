4c30aba71c4776525939e55a9d1b05ec825e427e
HL <>
2020-12-15 16:13:36 -0500

    Fix return again

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -48,7 +48,7 @@ contract SimpleStorage {
         emit StorageSet("addressPayable data stored successfully!");
     }
 
-    function setString(string memory x) public returns (string) {
+    function setString(string memory x) public returns (string memory) {
         storedString = x;
         emit StorageSet("string data stored successfully!");
         return storedString;
