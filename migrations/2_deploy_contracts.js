7bcc6b3c076d33a38a26505978ddf14bb684b7e3
HL <>
2020-12-16 12:27:07 -0500

    Revert "Add crowdfunding"
    This reverts commit 115fd1aebd2dc86b2ba8bd6a76e1d2c8a600392b.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,16 +5,9 @@ const MetaCoin3 = artifacts.require("MetaCoin3");
 const MetaCoin4 = artifacts.require("MetaCoin4");
 const MetaCoin5 = artifacts.require("MetaCoin5");
 const SimpleStorage = artifacts.require("SimpleStorage");
-const Crowdfunding = artifacts.require("Crowdfunding");
 
 const contracts = [
-  MetaCoin, 
-  MetaCoin2, 
-  MetaCoin3, 
-  MetaCoin4, 
-  MetaCoin5, 
-  SimpleStorage,
-  Crowdfunding
+  MetaCoin, MetaCoin2, MetaCoin3, MetaCoin4, MetaCoin5, SimpleStorage
 ];
 
 module.exports = function(deployer) {
